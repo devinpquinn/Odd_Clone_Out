606266dd4992bc8aaa3ca2e74d1fbaaaf8e4d895
Devin <devinpquinn4@gmail.com>
2026-05-08 19:57:29 -0400

    rotation interim

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public Transform platform;
+    public float rotationDuration = 0.5f;
+    public Ease rotationEase = Ease.InOutQuad;
+
+    private float _currentYRotation = 0f;
+    private Tween _rotateTween;
+
+    public void RotateClockwise()
+    {
+        _currentYRotation += 90f;
+        RotatePlatformTo(_currentYRotation);
+    }
+
+    public void RotateCounterClockwise()
+    {
+        _currentYRotation -= 90f;
+        RotatePlatformTo(_currentYRotation);
+    }
+
+    private void RotatePlatformTo(float yDegrees)
+    {
+        _rotateTween?.Kill();
+        _rotateTween = platform
+            .DORotate(new Vector3(0f, yDegrees, 0f), rotationDuration, RotateMode.FastBeyond360)
+            .SetEase(rotationEase);
+    }
+}
