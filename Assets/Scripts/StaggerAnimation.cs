560b4d0435789985cd92cbffd4893239c537db82
Devin <devinpquinn4@gmail.com>
2026-05-07 21:49:31 -0400

    blink stagger

--- Assets/Scripts/StaggerAnimation.cs
+++ Assets/Scripts/StaggerAnimation.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class StaggerAnimation : MonoBehaviour
 {
     public string clipName = "Idle_A";
+    public int layer = 0;
 
     private void Start()
     {
         Animator animator = GetComponent<Animator>();
         if (animator != null)
         {
-            animator.Play(clipName, 0, Random.value);
+            animator.Play(clipName, layer, Random.value);
         }
     }
 }
