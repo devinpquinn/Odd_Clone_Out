84b79b95e7559054aa105001cdb7810bc530213f
Devin <devinpquinn4@gmail.com>
2026-05-08 20:01:01 -0400

    interim

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -7,26 +7,23 @@ public class LevelController : MonoBehaviour
     public float rotationDuration = 0.5f;
     public Ease rotationEase = Ease.InOutQuad;
 
-    private float _currentYRotation = 0f;
     private Tween _rotateTween;
 
     public void RotateClockwise()
     {
-        _currentYRotation += 90f;
-        RotatePlatformTo(_currentYRotation);
+        RotatePlatformBy(90f);
     }
 
     public void RotateCounterClockwise()
     {
-        _currentYRotation -= 90f;
-        RotatePlatformTo(_currentYRotation);
+        RotatePlatformBy(-90f);
     }
 
-    private void RotatePlatformTo(float yDegrees)
+    private void RotatePlatformBy(float yDelta)
     {
         _rotateTween?.Kill();
         _rotateTween = platform
-            .DORotate(new Vector3(0f, yDegrees, 0f), rotationDuration, RotateMode.FastBeyond360)
+            .DORotate(new Vector3(0f, yDelta, 0f), rotationDuration, RotateMode.LocalAxisAdd)
             .SetEase(rotationEase);
     }
 }
