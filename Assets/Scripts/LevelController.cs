8a1829bd0946782b86d3af55a0c57f77b5ed44c9
Devin <devinpquinn4@gmail.com>
2026-05-08 20:22:38 -0400

    interim

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -12,19 +12,20 @@ public class LevelController : MonoBehaviour
 
     public void RotateClockwise()
     {
+        if (_rotateTween != null && _rotateTween.IsActive() && _rotateTween.IsPlaying()) return;
         _currentYRotation += 90f;
         RotatePlatformTo(_currentYRotation);
     }
 
     public void RotateCounterClockwise()
     {
+        if (_rotateTween != null && _rotateTween.IsActive() && _rotateTween.IsPlaying()) return;
         _currentYRotation -= 90f;
         RotatePlatformTo(_currentYRotation);
     }
 
     private void RotatePlatformTo(float yDegrees)
     {
-        _rotateTween?.Kill();
         _rotateTween = platform
             .DORotate(new Vector3(0f, yDegrees, 0f), rotationDuration)
             .SetEase(rotationEase);
