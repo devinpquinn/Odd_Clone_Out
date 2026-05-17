bb94c6cab269c81ca8a4eb23a6339791dcae09cb
Devin <devinpquinn4@gmail.com>
2026-05-17 13:49:04 -0400

    interim

--- Assets/Scripts/CreatureSpawner.cs
+++ Assets/Scripts/CreatureSpawner.cs
@@ -4,7 +4,9 @@ using System.Collections.Generic;
 public class CreatureSpawner : MonoBehaviour
 {
     public List<Transform> spawnPoints;
+    public Transform referenceSpawnPoint;
     public GameObject creaturePrefab;
+    public GameObject deviantPrefab;
     
     private void Start()
     {
@@ -13,11 +15,18 @@ public class CreatureSpawner : MonoBehaviour
     
     private void SpawnCreatures()
     {
+        Transform deviantSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+
+        float referenceYAngle = Random.Range(0, 4) * 90f;
+        Quaternion referenceRotation = referenceSpawnPoint.rotation * Quaternion.Euler(0f, referenceYAngle, 0f);
+        Instantiate(creaturePrefab, referenceSpawnPoint.position, referenceRotation, referenceSpawnPoint);
+
         foreach (Transform spawnPoint in spawnPoints)
         {
             float yAngle = Random.Range(0, 4) * 90f;
             Quaternion rotation = spawnPoint.rotation * Quaternion.Euler(0f, yAngle, 0f);
-            Instantiate(creaturePrefab, spawnPoint.position, rotation, spawnPoint);
+            GameObject prefab = (spawnPoint == deviantSpawnPoint) ? deviantPrefab : creaturePrefab;
+            Instantiate(prefab, spawnPoint.position, rotation, spawnPoint);
         }
     }
 }
