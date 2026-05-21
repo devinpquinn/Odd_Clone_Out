874b2f0449faa1604d42d07a43e25b4d7d17f327
Devin <devinpquinn4@gmail.com>
2026-05-21 16:31:54 -0400

    click to zap

--- Assets/Scripts/CreatureSelector.cs
+++ Assets/Scripts/CreatureSelector.cs
@@ -19,11 +19,12 @@ public class CreatureSelector : MonoBehaviour
     private void Update()
     {
         Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-        int creatureLayer = LayerMask.GetMask(creatureLayerName);
+        int creatureLayerMask = LayerMask.GetMask(creatureLayerName);
+        int creatureLayer = LayerMask.NameToLayer(creatureLayerName);
 
         SkinnedMeshRenderer hoveredRenderer = null;
 
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, creatureLayer))
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, creatureLayerMask))
         {
             if (!hit.collider.CompareTag(CreatureSpawner.tagCreatureReference))
             {
@@ -41,5 +42,42 @@ public class CreatureSelector : MonoBehaviour
             if (_currentOutlined != null)
                 _currentOutlined.renderingLayerMask |= _outlineMask;
         }
+
+        if (Mouse.current.leftButton.wasPressedThisFrame && _currentOutlined != null)
+        {
+            Collider creatureCollider = _currentOutlined.GetComponentInParent<Collider>();
+            if (creatureCollider == null)
+            {
+                creatureCollider = _currentOutlined.GetComponentInChildren<Collider>();
+            }
+
+            if (creatureCollider != null)
+            {
+                if (creatureCollider.CompareTag(CreatureSpawner.tagCreatureDeviant))
+                {
+                    Debug.Log("Clicked creature: Deviant clone");
+                }
+                else if (creatureCollider.CompareTag(CreatureSpawner.tagCreatureNormal))
+                {
+                    Debug.Log("Clicked creature: Normal clone");
+                }
+
+                GameObject creatureRoot = GetCreatureRoot(_currentOutlined.gameObject, creatureLayer);
+                _currentOutlined.renderingLayerMask &= ~_outlineMask;
+                _currentOutlined = null;
+                creatureRoot.SetActive(false);
+            }
+        }
+    }
+
+    private GameObject GetCreatureRoot(GameObject source, int creatureLayer)
+    {
+        Transform current = source.transform;
+        while (current.parent != null && current.parent.gameObject.tag != "SpawnPoint")
+        {
+            current = current.parent;
+        }
+
+        return current.gameObject;
     }
 }
