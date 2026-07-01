63489a12ad67c0dc3065dd6162fd336e8a20009d
Devin <devinpquinn4@gmail.com>
2026-07-01 19:07:00 -0400

    interim

---
+++ Assets/Scripts/DebugMenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DebugMenuController : MonoBehaviour
+{
+    public List<Stage> stages;
+    
+    public Transform debugMenuParent;
+    public GameObject debugStageObjPrefab;
+    public GameObject debugLevelButtonPrefab;
+}
