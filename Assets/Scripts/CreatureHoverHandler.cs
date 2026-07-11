3c731af7a3b79506755180a3e837620257c56327
Devin <devinpquinn4@gmail.com>
2026-07-10 22:04:59 -0400

    cleanup

--- Assets/Scripts/CreatureHoverHandler.cs
+++ Assets/Scripts/CreatureHoverHandler.cs
@@ -3,12 +3,8 @@ using UnityEngine.Events;
 
 public class CreatureHoverHandler : MonoBehaviour
 {
-    public SkinnedMeshRenderer skinnedMeshRenderer;
-    private string creatureLayerName = "Creature";
-    private string outlineLayerName = "Outline";
     public UnityEvent onHoverStart;
     public UnityEvent onHoverEnd;
-    private bool isHovered = false;
     
     public void StartHover()
     {
